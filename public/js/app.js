506a573186c2b95a25386e27c84395d006acc909
Juan Díaz <juan.diaz.12069@gmail.com>
2019-08-29 18:34:54 -0400

    add skycons functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+const skycons = new Skycons({"color": "gray"})
+const icon = document.getElementById('icon');
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -8,14 +10,17 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
+    skycons.remove('icon')
     fetch('/weather?address='+location).then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            return messageOne.textContent = data.error
-        }
-        messageOne.textContent = data.location
-        messageTwo.textContent = data.forecast
+        response.json().then((data) => {
+            if(data.error) {
+                return messageOne.textContent = data.error
+            }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+            skycons.add(icon, data.icon);
+            skycons.play();
+        })
     })
-})
 })
 
